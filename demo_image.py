92406a46b48659ac4a7027cb9b8e0e3e58ebf5dd
Kento Watanabe <kentowatanabe@KentonoMacBook-Pro.local>
2023-07-10 16:36:03 +0900

    鼻から首までの距離を計算

--- demo_image.py
+++ demo_image.py
@@ -1,16 +1,19 @@
 import cv2
-import matplotlib.pyplot as plt
-import copy
 import numpy as np
+import math
 import torch
 import sys
 import os
+from typing import List, Dict
+
+Point = Dict[str, float]
+# x: x_coord
+# y: y_coord
+# score: score
 
 path_pytorch_openpose: str = "pytorch-openpose"
 
 sys.path.append(f"{os.getcwd()}/{path_pytorch_openpose}")
-from src import model
-from src import util
 from src.body import Body
 
 body_estimation = Body(f"{path_pytorch_openpose}/model/body_pose_model.pth")
@@ -23,12 +26,14 @@ if oriImg is None:
     raise ValueError(f"the file {sys.argv[1]} could not open")
 
 candidate, subset = body_estimation(oriImg)
-canvas = copy.deepcopy(oriImg)
-canvas = util.draw_bodypose(canvas, candidate, subset)
 
-cv2.namedWindow("result", cv2.WINDOW_NORMAL)
-cv2.imshow('result', canvas)#一个窗口用以显示原视频
+def parse_point(cand) -> Point:
+    return {
+        "x": cand[0],
+        "y": cand[1],
+        "score": cand[2],
+    }
 
-cv2.waitKey()
-cv2.destroyAllWindows()
-
+nose: Point = parse_point(candidate[0])
+neck: Point = parse_point(candidate[1])
+print(math.dist([nose["x"], nose["y"]], [neck["x"], neck["y"]]))
