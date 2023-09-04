1ffd84f961ac46d152f91c28206409fee458a027
Kento Watanabe <kentowatanabe@KentonoMacBook-Pro.local>
2023-09-04 16:29:20 +0900

    docker導入

--- api/main.py
+++ api/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-import os
 import cv2
 import asyncio
-from demo_image import calc_neck_dist, calc_head_angle, save_file, remove_file
+# from demo_image import calc_neck_dist, calc_head_angle, save_file, remove_file
 from typing import Dict, List, Any
 import uvicorn
 
@@ -13,6 +12,10 @@ origins = [
     "https://localhost",
     "http://localhost:4200",
     "https://localhost:4200",
+    "http://localhost.justune.net",
+    "https://localhost.justune.net",
+    "http://localhost.justune.net:4200",
+    "https://localhost.justune.net:4200",
 ]
 
 app.add_middleware(
@@ -28,44 +31,44 @@ app.add_middleware(
 def get_hello_world():
     return {"Hello": "World"}
 
-@app.post("/score")
-async def get_posture_score(file: UploadFile = File(...)) -> Dict[str, float]:
-    file_name: str = save_file(file)
-    img = cv2.imread(file_name)
-    tasks: List[Any] = []
-    tasks.append(calc_neck_dist(img))
-    tasks.append(calc_head_angle(img))
-    [neck_len, head_angle] = await asyncio.gather(*tasks)
-    try:
-        remove_file(file_name)
-    except FileNotFoundError:
-        pass
-    return {
-        "neckLength": neck_len,
-        "headAngle": head_angle,
-    }
+# @app.post("/score")
+# async def get_posture_score(file: UploadFile = File(...)) -> Dict[str, float]:
+#     file_name: str = save_file(file)
+#     img = cv2.imread(file_name)
+#     tasks: List[Any] = []
+#     tasks.append(calc_neck_dist(img))
+#     tasks.append(calc_head_angle(img))
+#     [neck_len, head_angle] = await asyncio.gather(*tasks)
+#     try:
+#         remove_file(file_name)
+#     except FileNotFoundError:
+#         pass
+#     return {
+#         "neckLength": neck_len,
+#         "headAngle": head_angle,
+#     }
 
-@app.post("/neck")
-async def get_neck_dist(file: UploadFile = File(...)) -> float:
-    file_name: str = save_file(file)
-    img = cv2.imread(file_name)
-    dist: float = await calc_neck_dist(img)
-    try:
-        remove_file(file_name)
-    except FileNotFoundError:
-        pass
-    return dist
+# @app.post("/neck")
+# async def get_neck_dist(file: UploadFile = File(...)) -> float:
+#     file_name: str = save_file(file)
+#     img = cv2.imread(file_name)
+#     dist: float = await calc_neck_dist(img)
+#     try:
+#         remove_file(file_name)
+#     except FileNotFoundError:
+#         pass
+#     return dist
 
-@app.post("/head")
-async def get_head_angle(file: UploadFile = File(...)) -> float:
-    file_name: str = save_file(file)
-    img = cv2.imread(file_name)
-    try:
-        remove_file(file_name)
-    except FileNotFoundError:
-        pass
-    yaw: float = await calc_head_angle(img)
-    return yaw
+# @app.post("/head")
+# async def get_head_angle(file: UploadFile = File(...)) -> float:
+#     file_name: str = save_file(file)
+#     img = cv2.imread(file_name)
+#     try:
+#         remove_file(file_name)
+#     except FileNotFoundError:
+#         pass
+#     yaw: float = await calc_head_angle(img)
+#     return yaw
 
 if __name__ == "__main__":
     uvicorn.run(
@@ -73,6 +76,6 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=8000,
         reload=True,
-        ssl_keyfile="../ssl/localhost-key.pem",
-        ssl_certfile="../ssl/localhost.pem"
+        ssl_keyfile="../ssl/localhost.justune.net-key.pem",
+        ssl_certfile="../ssl/localhost.justune.net.cert.pem"
     )
---
+++ db/init/2_insert_dummy_user.sql
@@ -0,0 +1,6 @@
+SET CHARSET UTF8;
+CREATE DATABASE IF NOT EXISTS `posture_correction_db` DEFAULT CHARACTER SET utf8  /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `posture_correction_db`;
+
+INSERT INTO `users` (`name`) VALUES ("anonymous");
