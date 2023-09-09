08876f19c738547e807593a94fa6d0201fa0682e
Kento Watanabe <kentowatanabe@KentonoMacBook-Pro.local>
2023-09-10 00:48:07 +0900

    スペルミス直した

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -2,18 +2,18 @@ echo \
 "export const environment = {\n\
     production: true,\n\
     API_URL: \"$API_URL\" ?? \"http://api:8000\",\n\
-    API_ENDPOINT: \"$API_ENTDPOINT\" ?? \"/api/\",\n\
+    API_ENDPOINT: \"$API_ENDPOINT\" ?? \"/api/\",\n\
 };" > /app/src/environments/environment.ts
 
 echo \
 "export const environment = {\n\
     production: false,\n\
     API_URL: \"$API_URL\" ?? \"http://api:8000\",\n\
-    API_ENDPOINT: \"$API_ENTDPOINT\" ?? \"/api/\",\n\
+    API_ENDPOINT: \"$API_ENDPOINT\" ?? \"/api/\",\n\
 };" > /app/src/environments/environment.development.ts
 
 echo "{\n\
-    \"/api/*\": {\n\
+    \"$API_ENDPOINT*\": {\n\
         \"target\": \"$API_URL\",\n\
         \"secure\": false,\n\
         \"pathRewrite\": {\"^$API_ENDPOINT\" : \"/\"},\n\
