54fc82cbe0654c7909b7e846ec5e75a0cc9d1e70
Kento Watanabe <kentowatanabe@KentonoMacBook-Pro.local>
2023-09-13 20:48:19 +0900

    ユーザ名表示

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { UserFacade } from '../store/user/facade';
 import { Observable } from 'rxjs';
+import { User } from '../types/User';
 
 @Component({
   selector: 'app-header',
@@ -13,13 +14,14 @@ import { Observable } from 'rxjs';
 })
 export class HeaderComponent {
   isLoggedIn$: Observable<boolean>;
+  user$: Observable<User>;
 
   constructor(private userFacade: UserFacade) {
-    this.isLoggedIn$ = this.userFacade.isLoggedIn$
+    this.isLoggedIn$ = this.userFacade.isLoggedIn$;
+    this.user$ = this.userFacade.user$;
   }
 
   handleLogout() {
     this.userFacade.logout();
-    console.log(this.isLoggedIn$.subscribe(state => console.log(state)))
   }
 }
