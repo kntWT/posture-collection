dcf5104d737597797a239b81a65e2e95afca64da
Kento Watanabe <kentowatanabe@KentonoMacBook-Pro.local>
2023-09-18 16:05:59 +0900

    ログイン，post，キャリブレーション処理実装

--- external_estimator/human_posture_analysis_camera.py
+++ external_estimator/human_posture_analysis_camera.py
@@ -3,6 +3,13 @@ import time
 import math as m
 import mediapipe as mp
 
+import requests
+from requests.exceptions import HTTPError
+from requests.auth import HTTPBasicAuth
+import base64
+from typing import Dict, NoReturn
+import json
+
 
 # Calculate distance
 def findDistance(x1, y1, x2, y2):
@@ -14,7 +21,7 @@ def findDistance(x1, y1, x2, y2):
 def findAngle(x1, y1, x2, y2):
     theta = m.acos((y2 - y1) * (-y1) / (m.sqrt(
         (x2 - x1) ** 2 + (y2 - y1) ** 2) * y1))
-    degree = int(180 / m.pi) * theta
+    degree = 180 / m.pi * theta
     return degree
 
 
@@ -27,6 +34,48 @@ Feel free to get creative and customize as per your convenience.
 def sendWarning(x):
     pass
 
+def login(trial: int = 0) -> Dict:
+    if trial >= 5:
+        return {}
+    user_name: str = input()
+    password: str = input()
+    auth = HTTPBasicAuth(user_name, password)
+    try:
+        res = requests.get(f"{API_URL}/user/auth/", auth=auth)
+        res.raise_for_status()
+        user = res.json()
+        return user
+    except HTTPError as e:
+        print(e)
+        return login(trial + 1)
+    
+def calibrate(offset) -> NoReturn:
+    NECK_ANGLE_OFFSET = offset
+    data = json.dumps({"neck_angle_offset": NECK_ANGLE_OFFSET})
+    user_id: int = user["id"]
+    try:
+        res = requests.put(f"{API_URL}/user/calibration/external-posture/{user_id}", data)
+        res.raise_for_status()
+        return
+    except HTTPError as e:
+        print(e)
+        return
+    finally:
+        return
+
+def post(neck_angle: float) -> NoReturn:
+    if len(user) <= 0:
+        return
+    data = json.dumps({"user_id": user["id"], "neck_angle": neck_angle})
+    try:
+        res = requests.post(f"{API_URL}/external-posture/", data)
+        res.raise_for_status()
+        return
+    except HTTPError as e:
+        print(e)
+        return
+    finally:
+        return
 
 # =============================CONSTANTS and INITIALIZATIONS=====================================#
 # Initilize frame counters.
@@ -48,13 +97,18 @@ pink = (255, 0, 255)
 # Initialize mediapipe pose class.
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
+
+API_URL: str = "http://localhost:4201"
+NECK_ANGLE_OFFSET: float = 0
+user = {}
 # ===============================================================================================#
 
 
 if __name__ == "__main__":
+    user = login()
     cap = cv2.VideoCapture(1)
     cap.set(3, 640)
-    cap.set(4, 480)
+    cap.set(4, 960)
 
     while True:
         # Capture frames.
@@ -107,6 +161,11 @@ if __name__ == "__main__":
         # Calculate angles.
         neck_inclination = findAngle(l_shldr_x, l_shldr_y, l_ear_x, l_ear_y)
         torso_inclination = findAngle(l_hip_x, l_hip_y, l_shldr_x, l_shldr_y)
+        neck_angle: float = neck_inclination - NECK_ANGLE_OFFSET
+
+        # presse enter key
+        if cv2.waitKey(1) == 13:
+            calibrate(neck_inclination)
 
         # Draw landmarks.
         cv2.circle(image, (l_shldr_x, l_shldr_y), 7, yellow, -1)
@@ -124,16 +183,16 @@ if __name__ == "__main__":
 
         # Put text, Posture and angle inclination.
         # Text string for display.
-        angle_text_string = 'Neck : ' + str(int(neck_inclination)) + '  Torso : ' + str(int(torso_inclination))
+        angle_text_string = 'Neck : ' + str(int(neck_angle)) + '  Torso : ' + str(int(torso_inclination))
 
         # Determine whether good posture or bad posture.
         # The threshold angles have been set based on intuition.
-        if neck_inclination < 40 and torso_inclination < 10:
+        if neck_angle < 40 and torso_inclination < 10:
             bad_frames = 0
             good_frames += 1
             
             cv2.putText(image, angle_text_string, (10, 30), font, 0.9, light_green, 2)
-            cv2.putText(image, str(int(neck_inclination)), (l_shldr_x + 10, l_shldr_y), font, 0.9, light_green, 2)
+            cv2.putText(image, str(int(neck_angle)), (l_shldr_x + 10, l_shldr_y), font, 0.9, light_green, 2)
             cv2.putText(image, str(int(torso_inclination)), (l_hip_x + 10, l_hip_y), font, 0.9, light_green, 2)
 
             # Join landmarks.
@@ -147,7 +206,7 @@ if __name__ == "__main__":
             bad_frames += 1
 
             cv2.putText(image, angle_text_string, (10, 30), font, 0.9, red, 2)
-            cv2.putText(image, str(int(neck_inclination)), (l_shldr_x + 10, l_shldr_y), font, 0.9, red, 2)
+            cv2.putText(image, str(int(neck_angle)), (l_shldr_x + 10, l_shldr_y), font, 0.9, red, 2)
             cv2.putText(image, str(int(torso_inclination)), (l_hip_x + 10, l_hip_y), font, 0.9, red, 2)
 
             # Join landmarks.
@@ -172,6 +231,7 @@ if __name__ == "__main__":
         if bad_time > 180:
             sendWarning()
 
+        post(neck_inclination)
         # Display.
         cv2.imshow('MediaPipe Pose', image)
         if cv2.waitKey(5) & 0xFF == ord('q'):
