95aecb38f514b367296f3380e16a7d21c45bdaa8
Kento Watanabe <kentowatanabe@KentonoMacBook-Pro.local>
2023-10-25 20:07:02 +0900

    角度推定（一応実装完了）

--- client/src/app/video/plugins/DeviceMotionDetector.ts
+++ client/src/app/video/plugins/DeviceMotionDetector.ts
@@ -27,7 +27,7 @@ export class DeviceMotionDetector {
     osCorrection: number = 0; // to correct alpha value on Android device
     parmitted: boolean = false;
     updatedAt: Date | null = null;
-    k: number = 0.95;
+    k: number = 0.9;
 
     constructor(){
     }
@@ -39,7 +39,7 @@ export class DeviceMotionDetector {
             requestPermission().then(res => {
                 if (res === 'granted') {
                     this.parmitted = true;
-                    this.starteMotionDetection();
+                    this.startMotionDetection();
                 } else {
                     alert("動作と方向へのアクセスを許可してください");
                 }
@@ -48,7 +48,7 @@ export class DeviceMotionDetector {
             });
         } else {
             this.parmitted = true;
-            this.starteMotionDetection();
+            this.startMotionDetection();
         }
 
         if(navigator && navigator.userAgent) {
@@ -56,7 +56,7 @@ export class DeviceMotionDetector {
         }
     }
 
-    starteMotionDetection(){
+    startMotionDetection(){
         if(!window || !this.parmitted) return;
 
         window.addEventListener('devicemotion', e => {
@@ -70,44 +70,74 @@ export class DeviceMotionDetector {
             this.motion.acceleration.rotationY = e.rotationRate?.beta;
             this.motion.acceleration.rotationZ = e.rotationRate?.gamma;
 
-            this.motion.x += (e.acceleration?.x ?? 0) * e.interval ** 2;
-            this.motion.y += (e.acceleration?.y ?? 0) * e.interval ** 2;
-            this.motion.z += (e.acceleration?.z ?? 0) * e.interval ** 2;
+            // this.motion.x += (e.acceleration?.x ?? 0) * (e.interval ** 2);
+            this.motion.x = this.lowPassFilter(this.motion.x, (e.acceleration?.x ?? 0) * e.interval);
+            this.motion.y = this.lowPassFilter(this.motion.y, (e.acceleration?.y ?? 0) * e.interval);
+            this.motion.z = this.lowPassFilter(this.motion.z, (e.acceleration?.z ?? 0) * e.interval);
 
-            const pitch = Math.cos(this.motion.roll)*(e.rotationRate?.alpha ?? 0)
+            const pitchAcc = Math.cos(this.motion.roll)*(e.rotationRate?.alpha ?? 0)
                 - Math.sin(this.motion.roll)*(e.rotationRate?.beta ?? 0);
-            this.motion.pitch = this.conplementaryFilter(
-                this.motion.pitch,
-                pitch,
-                this.motion.acceleration.gravityZ ?? 0,
-                this.motion.acceleration.gravityY ?? 0,
-                e.interval
+            const pitchGrav = this.normalizeDegree(
+                this.radianToDegree(
+                    Math.atan2(
+                        this.motion.acceleration.gravityZ ?? 0,
+                        this.motion.acceleration.gravityY ?? 0
+                    )
+                )
             );
-            const yaw = Math.sin(this.motion.roll) / Math.cos(this.motion.pitch) * (e.rotationRate?.alpha ?? 0)
+            const yawAcc = Math.sin(this.motion.roll) / Math.cos(this.motion.pitch) * (e.rotationRate?.alpha ?? 0)
                 + Math.cos(this.motion.roll) / Math.cos(this.motion.pitch) * (e.rotationRate?.beta ?? 0);
-            this.motion.yaw = this.conplementaryFilter(
-                this.motion.yaw,
-                yaw,
-                this.motion.acceleration.gravityX ?? 0,
-                this.motion.acceleration.gravityZ ?? 0,
-                e.interval
+            const yawGrav = this.normalizeDegree(
+                this.radianToDegree(
+                    Math.atan2(
+                        this.motion.acceleration.gravityX ?? 0,
+                        this.motion.acceleration.gravityZ ?? 0
+                    )
+                )
             );
-            const roll = (e.rotationRate?.gamma ?? 0)
+            const rollAcc = (e.rotationRate?.gamma ?? 0)
                 + Math.sin(this.motion.roll) * Math.sin(this.motion.pitch) / Math.cos(this.motion.pitch) * (e.rotationRate?.alpha ?? 0)
                 + Math.cos(this.motion.roll) * Math.sin(this.motion.pitch) / Math.cos(this.motion.pitch) * (e.rotationRate?.beta ?? 0);
-            this.motion.roll = this.conplementaryFilter(
-                this.motion.roll,
-                roll,
-                this.motion.acceleration.gravityY ?? 0,
-                this.motion.acceleration.gravityX ?? 0,
-                e.interval
+            const rollGrav = this.normalizeDegree(
+                this.radianToDegree(
+                    Math.atan2(
+                        this.motion.acceleration.gravityY ?? 0,
+                        this.motion.acceleration.gravityX ?? 0
+                    )
+                ) - 90
             );
+            // this.motion.pitch = this.complementaryFilter(this.motion.pitch, pitchAcc, pitchGrav, e.interval);
+            this.motion.pitch = this.lowPassFilter(this.motion.pitch, pitchGrav);
+            // this.motion.yaw = this.complementaryFilter(this.motion.yaw, yawAcc, yawGrav, e.interval);
+            this.motion.yaw = this.lowPassFilter(this.motion.yaw, yawGrav);
+            // this.motion.roll = this.complementaryFilter(this.motion.roll, rollAcc, rollGrav, e.interval);
+            this.motion.roll = this.lowPassFilter(this.motion.roll, rollGrav);
 
             this.updatedAt = new Date();
         });
     }
 
-    conplementaryFilter(ptheta: number, theta: number, gravityX: number, gravityY: number, dt: number): number {
-        return this.k*(ptheta + theta*dt) + (1-this.k)*Math.atan2(gravityY, gravityX);
+    // 相補フィルタ
+    complementaryFilter(ptheta: number, thetaA: number, thetaB: number, dt: number): number {
+        return this.k*(ptheta + thetaA*dt) + (1-this.k)*thetaB;
+    }
+
+    //ローパスフィルタ
+    lowPassFilter(pvalue: number, value: number): number {
+        return this.k*pvalue + (1-this.k)*value;
+    }
+
+    degreeToRadian(degree: number): number {
+        return degree * Math.PI / 180;
+    }
+
+    radianToDegree(radian: number): number {
+        return radian * 180 / Math.PI;
+    }
+
+    // ~ -180, 180 ~ -> -180 ~ 180
+    normalizeDegree(degree: number): number {
+        const sign = degree >= 0 ? 1 : -1;
+        return (180 - Math.abs(degree)) * sign;
     }
 }
